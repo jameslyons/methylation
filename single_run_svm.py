f85812b8909caa44f800b2f0262ab9f4ae01f4f6
Rhys Heffernan <rhysheffernan@gmail.com>
2018-05-28 20:38:53 +1000

    now runs over the full list of REX files and prints predictions for each.

--- single_run_svm.py
+++ single_run_svm.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 import re
 import numpy as np
 from scipy.optimize import minimize
+import os
 
-#fileName = '/home/rhys/work/forElgit/RexFiles/HOXD12_Sample_Group_4b_61oC_140617.rex'
-fileName='/home/rhys/work/forElgit/RexFiles/DPYS_PLATE_8_60oC_120517.rex'
 
 def feat(control, controlLabel, exp):
     listzpc = []
@@ -24,13 +23,14 @@ def feat(control, controlLabel, exp):
 
     listFeat = []
     for singleExp in exp:
-        fun = lambda x: np.sum((singleExp - ((1-x)*meanZpc) - x*meanHpc)**2)
-        #res = minimize(fun, 0.5, bounds=bnds)
+        fun = lambda x: np.sum((singleExp - ((1 - x) * meanZpc) - x * meanHpc) ** 2)
+        # res = minimize(fun, 0.5, bounds=bnds)
         res = minimize(fun, 0.5)
 
         listFeat.append(res.x[0])
 
     return listFeat
+
 
 def label(controlLabel):
     listLabel = []
@@ -39,75 +39,97 @@ def label(controlLabel):
     return listLabel
 
 
-# read the rex file to get the data.
-try:
-    control, controlQpcr, exp, expQpcr, controlLabel, expLabel, numControl, numExp, temperature = loadRex(fileName)
-except:
-    print("error reading", fileName)
-    raise
+#fileName = '/home/rhys/work/forElgit/RexFiles/HOXD12_Sample_Group_4b_61oC_140617.rex'
 
-featControl=feat(control, controlLabel, control[2:])
-featExp    =feat(control, controlLabel, exp)
+FNameREXList = 'RexList.txt'
+with open(FNameREXList) as fp:
+    listREXNames = fp.read().splitlines()
 
-labelControl = label(controlLabel[2:])
+for REXName in listREXNames:
+    #fileName='/home/rhys/work/forElgit/RexFiles/DPYS_PLATE_8_60oC_120517.rex'
 
-for C in [0.001,0.01,0.1,0.2,0.5,0.8,1,2,5]:
-    for ESPILON in [0.001,0.01,0.1,0.2,0.5,0.8,1,2,5]:
+    FNameREX = '/home/rhys/work/forElgit/RexFiles/'+REXName
+    print(FNameREX)
 
-        mod = SVR(kernel='rbf', C=C, epsilon=ESPILON)
-        mod.fit(np.matrix(featControl).T, np.array(labelControl))
-        pred = mod.predict(np.matrix(featExp).T)
-        #print(C, ESPILON, pred)
+    FNameOutput = os.path.splitext(os.path.basename(FNameREX))[0]+".svm"
 
 
+    # read the rex file to get the data.
+    try:
+        control, controlQpcr, exp, expQpcr, controlLabel, expLabel, numControl, numExp, temperature = loadRex(FNameREX)
+    except:
+        print("error reading", FNameREX)
+        raise
 
-gridERR =[]
-gridC = []
-gridEPSILON = []
+    featControl=feat(control, controlLabel, control[2:])
+    featExp    =feat(control, controlLabel, exp)
 
-listC = list(np.linspace(0.1, 15, 20))
-listEPSILON = list(np.linspace(0, 15,20))
+    labelControl = label(controlLabel[2:])
 
-for C in listC:
-    for EPSILON in listEPSILON:
-        #print(C,EPSILON)
-        gridC.append(C)
-        gridEPSILON.append(EPSILON)
+    '''
+    for C in [0.001,0.01,0.1,0.2,0.5,0.8,1,2,5]:
+        for ESPILON in [0.001,0.01,0.1,0.2,0.5,0.8,1,2,5]:
 
-        accCV = []
-        # leave one out CV
-        for i in range(len(featControl)):
-            labelTrain = [a for j,a in enumerate(labelControl) if j!=i]
-            labelTest = labelControl[i]/100
-            featTrain = [a for j,a in enumerate(featControl) if j!=i]
-            featTest = featControl[i]/100
+            mod = SVR(kernel='rbf', C=C, epsilon=ESPILON)
+            mod.fit(np.matrix(featControl).T, np.array(labelControl))
+            pred = mod.predict(np.matrix(featExp).T)
+            #print(C, ESPILON, pred)
+    '''
 
-            modCV = SVR(kernel='rbf', C=C, epsilon=EPSILON)
-            #print(featTrain, labelTrain)
-            modCV.fit(np.matrix(featTrain).T,np.array(labelTrain))
-            #print(featTest)
-            predCV = modCV.predict(np.matrix(featTest).T)
 
-            accCV.append((labelTest-predCV)**2)
+    gridERR =[]
+    gridC = []
+    gridEPSILON = []
 
-        gridERR.append(np.mean(accCV))
+    listC = list(np.linspace(0.1, 15, 20))
+    listEPSILON = list(np.linspace(0, 15,20))
 
-'''
-plt.figure()
-plt.scatter(gridC, gridEPSILON, c=gridERR)
-plt.xlabel('C')
-plt.ylabel('Epsilon')
-plt.colorbar()
-plt.show()
-'''
+    for C in listC:
+        for EPSILON in listEPSILON:
+            #print(C,EPSILON)
+            gridC.append(C)
+            gridEPSILON.append(EPSILON)
 
-minERRIdx = np.argmin(gridERR)
-minC = gridC[minERRIdx]
-minEPSILON = gridEPSILON[minERRIdx]
+            accCV = []
+            # leave one out CV
+            for i in range(len(featControl)):
+                labelTrain = [a for j,a in enumerate(labelControl) if j!=i]
+                labelTest = labelControl[i]/100
+                featTrain = [a for j,a in enumerate(featControl) if j!=i]
+                featTest = featControl[i]/100
 
-mod = SVR(kernel='rbf', C=minC, epsilon=minEPSILON)
-mod.fit(np.matrix(featControl).T, np.array(labelControl))
-predExp = mod.predict(np.matrix(featExp).T)
+                modCV = SVR(kernel='rbf', C=C, epsilon=EPSILON)
+                #print(featTrain, labelTrain)
+                modCV.fit(np.matrix(featTrain).T,np.array(labelTrain))
+                #print(featTest)
+                predCV = modCV.predict(np.matrix(featTest).T)
 
-print(predExp)
+                accCV.append((labelTest-predCV)**2)
+
+            gridERR.append(np.mean(accCV))
+
+    '''
+    plt.figure()
+    plt.scatter(gridC, gridEPSILON, c=gridERR)
+    plt.xlabel('C')
+    plt.ylabel('Epsilon')
+    plt.colorbar()
+    plt.show()
+    '''
+
+    minERRIdx = np.argmin(gridERR)
+    minC = gridC[minERRIdx]
+    minEPSILON = gridEPSILON[minERRIdx]
+
+    mod = SVR(kernel='rbf', C=minC, epsilon=minEPSILON)
+    mod.fit(np.matrix(featControl).T, np.array(labelControl)/100)
+    predExp = mod.predict(np.matrix(featExp).T)*100
+
+    #print(predExp)
+
+    strTemp = "\n".join([str(a) for a in predExp])
+    #print(strTemp)
+
+    with open(FNameOutput, 'w') as fpPred:
+        fpPred.write(strTemp)
 
